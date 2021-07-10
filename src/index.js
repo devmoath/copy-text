fe4fb360374e2bf5f9ad04f67c36d6708d3cab41
Moath <moath.alhajrii@gmail.com>
2021-07-10 19:57:16 +0300

    initial release

---
+++ src/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} text
+ */
+function copy(text) {
+    /* Create textarea element and give it the text as innerText */
+    const textarea = document.createElement('textarea');
+    textarea.innerText = text;
+
+    /* Add the element to the DOM */
+    document.body.appendChild(textarea);
+
+    /* Select the the textarea text */
+    textarea.select();
+    textarea.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Execute copy command to copy the selected text */
+    document.execCommand('copy');
+
+    /* Finally, remove the textarea element from DOM */
+    textarea.remove();
+}
+
+module.exports = copy;
